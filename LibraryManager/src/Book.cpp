a642e5a19e92d6588c8830b93c8b4ee1a61302a7
Larisa <you@example.com>
2025-10-25 18:19:39 +0300

    clasa Book + CmakeLists.txt

---
+++ LibraryManager/src/Book.cpp
@@ -0,0 +1,56 @@
+#include "Book.hpp"
+
+
+Book::Book(const char* t, const char* a, int y)
+    : title(nullptr), author(nullptr), year(y)
+{
+    std::cout << "[Book ctor] Creating book: " << t << "\n";
+
+    title = new char[strlen(t) + 1];
+    strcpy(title, t);
+
+    author = new char[strlen(a) + 1];
+    strcpy(author, a);
+}
+
+Book::~Book() {
+    std::cout << "[Book dtor] Deleting book: " << (title ? title : "null") << "\n";
+    delete[] title;
+    delete[] author;
+}
+
+Book::Book(const Book& other)
+    : year(other.year)
+{
+    std::cout << "[Book copy ctor] Copying book: " << other.title << "\n";
+
+    title = new char[strlen(other.title) + 1];
+    strcpy(title, other.title);
+
+    author = new char[strlen(other.author) + 1];
+    strcpy(author, other.author);
+}
+
+Book::Book(Book&& other) noexcept
+    : title(other.title), author(other.author), year(other.year)
+{
+    std::cout << "[Book move ctor] Moving book: " << (other.title ? other.title : "null") << "\n";
+    other.title = nullptr;
+    other.author = nullptr;
+    other.year = 0;
+}
+
+const char* Book::getTitle() const { return title; }
+const char* Book::getAuthor() const { return author; }
+int Book::getYear() const { return year; }
+
+
+void Book::setTitle(const char* newTitle) {
+    delete[] title;
+    title = new char[strlen(newTitle) + 1];
+    strcpy(title, newTitle);
+}
+
+void Book::print() const {
+    std::cout << "Book: " << title << " by " << author << " (" << year << ")\n";
+}
---
+++ LibraryManager/src/Book.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <cstring>
+
+class Book {
+private:
+    char* title;   
+    char* author;  
+    int year;
+
+public:
+    Book(const char* t = "Unknown", const char* a = "Unknown", int y = 0);
+
+    ~Book();
+
+    Book(const Book& other);
+
+    Book(Book&& other) noexcept;
+
+    const char* getTitle() const;
+    const char* getAuthor() const;
+    int getYear() const;
+
+    void setTitle(const char* newTitle);
+
+    void print() const;
+};
